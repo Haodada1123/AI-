50320fcc3e9325cce7ba649e1bfe8bc2aefb8056
Haodada1123 <809570145@qq.com>
2026-04-06 11:54:25 +0800

    wancheng 5,6,7,8duiyingde gongneng

--- backend/web/views/friend/message/chat/chat.py
+++ backend/web/views/friend/message/chat/chat.py
@@ -1,6 +1,5 @@
 import json
 from http.client import responses
-from pprint import pprint
 
 from django.http import StreamingHttpResponse
 from langchain_core.messages import HumanMessage, BaseMessageChunk, SystemMessage, AIMessage
@@ -13,6 +12,8 @@ from rest_framework.permissions import IsAuthenticated
 from web.models import Friend, Message
 from web.models.friend import SystemPrompt
 from web.views.friend.message.chat.graph import ChatGraph
+from web.views.friend.message.memory.update import update_memory
+
 
 class SSERenderer(BaseRenderer):
     media_type = 'text/event-stream'
@@ -27,6 +28,7 @@ def add_system_prompt(state, friend):
     for sp in system_prompts:
         prompt += sp.prompt
     prompt += f'\n【角色性格】\n{friend.character.profile}\n'
+    prompt += f'【长期记忆】\n{friend.memory}\n'
     return {'messages': [SystemMessage(prompt)] + msgs}
 
 def add_recent_messages(state, friend):
@@ -93,6 +95,9 @@ class MessageChatView(APIView):
                 output_tokens=output_tokens,
                 total_tokens=total_tokens,
             )
+            if Message.objects.filter(friend=friend).count() % 1 == 0:
+                update_memory(friend)
+
 
         response = StreamingHttpResponse(event_stream(), content_type="text/event-stream")
         response['Cache-Control'] = 'no-cache'
